0d95a3ae3bef7c06ad6d7cfcd9582d9498a54c3a
Niranjan Dorage <niranjandorage@gmail.com>
2024-01-14 00:04:40 +0530

    bottom component added

--- lib/screens/categories/categories.dart
+++ lib/screens/categories/categories.dart
@@ -1,11 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:ostello_task/components/logical_components.dart';
+import 'package:ostello_task/theme/assets_strings.dart';
 
 import '../../theme/app_color.dart';
 import '../../theme/app_font.dart';
 
 class Categories extends StatelessWidget {
-  const Categories({super.key});
+  const Categories({Key? key});
 
   @override
   Widget build(BuildContext context) {
@@ -28,8 +29,9 @@ class Categories extends StatelessWidget {
                     height: 40,
                     margin: const EdgeInsets.only(left: 16),
                     decoration: BoxDecoration(
-                        color: AppColorScheme().whiteColor,
-                        borderRadius: BorderRadius.circular(100)),
+                      color: AppColorScheme().whiteColor,
+                      borderRadius: BorderRadius.circular(100),
+                    ),
                     child: Icon(
                       Icons.navigate_before_rounded,
                       size: 25,
@@ -42,8 +44,10 @@ class Categories extends StatelessWidget {
                 ),
                 Text(
                   "Categories",
-                  style: FontThemeClass()
-                      .appBarText(context, color: AppColorScheme().whiteColor),
+                  style: FontThemeClass().appBarText(
+                    context,
+                    color: AppColorScheme().whiteColor,
+                  ),
                 )
               ],
             ),
@@ -55,34 +59,73 @@ class Categories extends StatelessWidget {
               child: TextFormField(
                 cursorColor: AppColorScheme().primaryColor,
                 decoration: InputDecoration(
-                  contentPadding:
-                      const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+                  contentPadding: const EdgeInsets.symmetric(
+                    horizontal: 15,
+                    vertical: 12,
+                  ),
                   border: const OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(15),
-                      ),
-                      borderSide: BorderSide.none),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(15),
+                    ),
+                    borderSide: BorderSide.none,
+                  ),
                   fillColor: AppColorScheme().whiteColor,
                   filled: true,
                   focusColor: AppColorScheme().whiteColor,
-                  hintText: 'Student Email ID',
-                  hintStyle: FontThemeClass().medium(context,
-                      fontWeight: FontWeight.w400, color: Colors.grey),
+                  hintText: 'Search',
+                  hintStyle: FontThemeClass().medium(
+                    context,
+                    fontWeight: FontWeight.w400,
+                    color: Colors.grey,
+                  ),
                 ),
               ),
             ),
           ],
         ),
       ),
-      body: GridView.builder(
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            GridView.builder(
+              shrinkWrap: true,
+              physics: NeverScrollableScrollPhysics(),
+              gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+              ),
+              itemCount: LogicalComponents().categorieList.length,
+              itemBuilder: (BuildContext context, int index) {
+                var currentCategorie = LogicalComponents().categorieList[index];
+                return currentCategorie;
+              },
+            ),
+            Stack(
+              children: [
+                Container(
+                  margin:
+                      const EdgeInsets.symmetric(horizontal: 18, vertical: 50),
+                  width: double.infinity,
+                  height: 121,
+                  decoration: BoxDecoration(
+                      color: AppColorScheme().primaryColor,
+                      borderRadius: BorderRadius.circular(20)),
+                ),
+                Row(
+                  children: [
+                    Container(
+                      margin: EdgeInsets.only(top: 20),
+                      width: 164,
+                      child: Image(image: AssetImage(AssetName.girl)),
+                    )
+                  ],
+                )
+              ],
+            ),
+            SizedBox(
+              height: 20,
+            ),
+          ],
         ),
-        itemCount: 8,
-        itemBuilder: (BuildContext context, int index) {
-          var currentCategorie = LogicalComponents().categorieList[index];
-          return currentCategorie;
-        },
       ),
     );
   }
--- lib/theme/assets_strings.dart
+++ lib/theme/assets_strings.dart
@@ -1,5 +1,6 @@
 class AssetName {
   static String abcd = "assets/images/abcd.png";
+  static String girl = "assets/images/girl.png";
   static String coin = "assets/images/coin.png";
   static String profile = "assets/images/profile.png";
   static String course = "assets/images/coursePreview.png";
@@ -9,14 +10,13 @@ class AssetName {
   static String enrolled1 = "assets/images/enrolled1.png";
   static String enrolled2 = "assets/images/enrolled2.png";
   static String enrolled3 = "assets/images/enrolled3.png";
-  static String review= "assets/images/review_girl.png";
-  static String marketing= "assets/images/window.png";
-  static String development= "assets/images/coding.png";
-  static String design= "assets/images/coursePreview.png";
-  static String foreing= "assets/images/abcd.png";
-  static String finance= "assets/images/money.png";
-  static String business= "assets/images/calculator.png";
-  static String psychology= "assets/images/brain.png";
-  static String economics= "assets/images/earth.png";
-
+  static String review = "assets/images/review_girl.png";
+  static String marketing = "assets/images/window.png";
+  static String development = "assets/images/coding.png";
+  static String design = "assets/images/coursePreview.png";
+  static String foreing = "assets/images/abcd.png";
+  static String finance = "assets/images/money.png";
+  static String business = "assets/images/calculator.png";
+  static String psychology = "assets/images/brain.png";
+  static String economics = "assets/images/earth.png";
 }
